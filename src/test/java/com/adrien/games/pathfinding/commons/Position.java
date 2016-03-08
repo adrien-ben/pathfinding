a353abf556780f4760be8df90a7e157d03d3bc0f
Adrien Bennadji <adrien-ben@users.noreply.github.com>
2016-03-08 16:22:22 +0100

    removes useless import

--- src/test/java/com/adrien/games/pathfinding/commons/Position.java
+++ src/test/java/com/adrien/games/pathfinding/commons/Position.java
@@ -3,8 +3,6 @@ package com.adrien.games.pathfinding.commons;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-import java.util.Date;
-
 /**
  * Class representing a position in a path.
  */
